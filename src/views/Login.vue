39cd9f5094fe3a343cf0e1c97316cc7cee24f30d
lbbitweb <1548362885@qq.com>
2019-06-09 15:28:37 +0800

    这是一个初始化页面

---
+++ src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="login">
+        <el-form ref="form" :model="form">
+            <el-form-item>
+                <el-input v-model="form.username"></el-input>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            form:{
+                username: '',
+                password: ''
+            }
+        }
+    },
+}
+</script>
+<style lang="sass" scoped>
+
+</style>
+
